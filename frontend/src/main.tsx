29dcc63bc4d0853012477e51fff8fedbe919b62b
Stefan-Masic <stefan1nonly@gmail.com>
2025-12-06 12:10:22 -0500

    debugging UI Overhaul for node

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,23 +10,18 @@ import "./styles/ui.css";
 import "./styles/shell.css";
 import "./styles/auth.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./auth/AuthContext";
 import { ThemeProvider } from "./theme/ThemeProvider";
 import { ToastProvider } from "./components/ui/ToastProvider";
 
-const qc = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={qc}>
-      <AuthProvider>
-        <ThemeProvider>
-          <ToastProvider>
-            <App />
-          </ToastProvider>
-        </ThemeProvider>
-      </AuthProvider>
-    </QueryClientProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </ThemeProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
